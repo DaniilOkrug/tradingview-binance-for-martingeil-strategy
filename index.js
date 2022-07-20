a2a32f141430a68651cd74bf401a4fd4be7838e0
Daniil <daniil.okrug@gmail.com>
2022-07-20 20:40:11 +0300

    cors added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 
 const router = require("./router/index");
 
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+    optionSuccessStatus: 200
+}));
 app.use("/api", router);
 
 const start = async () => {
